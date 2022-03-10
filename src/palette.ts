276c043e8d8ac62bcb246499e612a75118505dc8
Pedro Henrique <pyter.dev@gmail.com>
2022-03-10 15:26:23 -0300

    refactor(color-palette): missing theme palettes added

--- src/palette.ts
+++ src/palette.ts
@@ -34,6 +34,83 @@ const defaultThemePalette = {
   },
 }
 
+const softThemePalette = {
+  base: {
+    background: '#2A2A2A',
+    foreground: '#E2E2E2',
+  },
+  additional: {
+    purple: '#C0A3DD',
+    orange: '#F8B24D',
+    darkGreen: '#315545',
+    gray: {
+      900: '#232323',
+      800: '#2A2A2A',
+      700: '#2E2E2E',
+      500: '#505050',
+    },
+  },
+  ansi: {
+    black: '#121212',
+    blue: '#9BC9EC',
+    cyan: '#8DE2E2',
+    green: '#41B883',
+    magenta: '#E676AB',
+    red: '#E9747A',
+    white: '#E2E2E2',
+    yellow: '#F8F893',
+    brightBlack: '#505050',
+    brightBlue: '#79B4E2',
+    brightCyan: '#66D5D7',
+    brightGreen: '#60C364',
+    brightMagenta: '#E25C98',
+    brightRed: '#F55258',
+    brightWhite: '#EFEFEF',
+    brightYellow: '#F7F740',
+  },
+}
+
+const intenseThemePalette = {
+  base: {
+    background: '#131715',
+    foreground: '#E2E2E2',
+  },
+  additional: {
+    purple: '#A87CCF',
+    orange: '#F6A227',
+    darkGreen: {
+      900: '#121413',
+      800: '#204434',
+    },
+    gray: {
+      900: '#171717',
+      800: '#272727',
+      700: '#323232',
+      500: '#505050',
+    },
+  },
+  ansi: {
+    black: '#121212',
+    blue: '#79B4E2',
+    cyan: '#50CCD0',
+    green: '#41B883',
+    magenta: '#E25698',
+    red: '#F55258',
+    white: '#E2E2E2',
+    yellow: '#F4F564',
+    brightBlack: '#505050',
+    brightBlue: '#63A4DD',
+    brightCyan: '#45C2CA',
+    brightGreen: '#41B847',
+    brightMagenta: '#E14985',
+    brightRed: '#FB3F3F',
+    brightWhite: '#FEFEFE',
+    brightYellow: '#F7E100',
+  },
+}
+
 export {
-  defaultThemePalette
+  defaultThemePalette,
+  softThemePalette,
+  intenseThemePalette
 }
