69e7bca647fb4de60d7de354f8fbd07cc3f5304a
Pedro Henrique <pyter.dev@gmail.com>
2022-03-10 14:58:47 -0300

    chore(color-palette): color palette updated

--- src/palette.ts
+++ src/palette.ts
@@ -1,35 +1,39 @@
-const palette = {
-  default: {
-    ansi: {
-      black: '#121212',
-      blue: '#79B4E2',
-      cyan: '#50CCD0',
-      green: '#41B883',
-      magenta: '#E25698',
-      red: '#F55258',
-      white: '#E2E2E2',
-      yellow: '#F4F564',
-      brightBlack: '#505050',
-      brightBlue: '#63A4DD',
-      brightCyan: '#45C2CA',
-      brightGreen: '#41B847',
-      brightMagenta: '#E14985',
-      brightRed: '#FB3F3F',
-      brightWhite: '#FEFEFE',
-      brightYellow: '#F7E100',
+const defaultThemePalette = {
+  base: {
+    background: '#171717',
+    foreground: '#E2E2E2',
+  },
+  additional: {
+    purple: '#A87CCF',
+    orange: '#F6A227',
+    darkGreen: '#204434',
+    gray: {
+      900: '#171717',
+      800: '#272727',
+      700: '#323232',
+      500: '#505050',
     },
-    additional: {
-      purple: '#A87CCF',
-      orange: '#F6A227',
-      darkGreen: '#204434',
-      gray: {
-        900: '#171717',
-        800: '#272727',
-        700: '#323232',
-        500: '#505050',
-      },
-    },
+  },
+  ansi: {
+    black: '#121212',
+    blue: '#79B4E2',
+    cyan: '#50CCD0',
+    green: '#41B883',
+    magenta: '#E25698',
+    red: '#F55258',
+    white: '#E2E2E2',
+    yellow: '#F4F564',
+    brightBlack: '#505050',
+    brightBlue: '#63A4DD',
+    brightCyan: '#45C2CA',
+    brightGreen: '#41B847',
+    brightMagenta: '#E14985',
+    brightRed: '#FB3F3F',
+    brightWhite: '#FEFEFE',
+    brightYellow: '#F7E100',
   },
 }
 
-export { palette }
+export {
+  defaultThemePalette
+}
